69af2843d52575450f236f1ec9028c7529b08448
vuvu31 <vulambaovan31@gmail.com>
2020-07-21 14:21:25 +0700

    1st: first upload

--- src/mathutil/MathUtil.java
+++ src/mathutil/MathUtil.java
@@ -19,6 +19,51 @@ public class MathUtil {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("5! = " + computeFactorial(5));
+
+        //khi ta viết code xong, ta/dev test xem hàm /class/app chạy có
+        //đúng k, việc này gọi là UniTest, test mức hàm, mức class, mức đơn vị
+        // test bằng cách đưa data vào hàm, class, chạy hàm xem kết quả trả về
+        //(actual value) rồi ta so kết quả trả về có đúng như ta đã mong
+        // đợi/ tính trc đó, mong đợi: expected value
+        //nếu ko bằng, hàm sai rồi, hoặc ta mong đợi sai
+        //trong main() ta sout() kết quả actual so sánh với mong đợi expected
+        // việc này gọi là kiểm thử = mắt, tự dùng mắt so sánh và luận kq
+        //expected: 1 , t hy vọng hàm m chạy ói về cho t con số 1
+        // nếu khi chạy m k in ra 1 mà in ra 10 (vd), 10 đc gọi là actual
+        System.out.println("1! = " + computeFactorial(1));
+
+        //expected: 1 hy vọng hàm tính 01 sẽ ra 1, thực tế (actual) phải chạy đã
+        System.out.println("0! = " + computeFactorial(0));
+
+        //expected: 2
+        System.out.println("2! = " + computeFactorial(2));
+
+        //expected: 6
+        System.out.println("3! = " + computeFactorial(3));
+
+        //expected:  chửi cà chớn nếu đưa vào cà chớn
+        System.out.println("-5! = " + computeFactorial(-5));
+        
+        //cách test bằng mắt có rủi ro nhỏ: tự so sánh data, luận, nhiều kq quá 
+        //dễ bị lỗi
+        //trong giang hồ ngta độ ra 1 kĩ thuật để test app/hàm chạy có đúng k
+        //bằng cách; gọi hàm với data đưa vào, lấy kq actual tự so sánh luôn với
+        //expected, nếu đúng, khớp, bằng, in ra màu xanh
+        //              sai, lệch, k =. in ra màu đỏ
+        //và quan trong hơn
+        // nếu gọi hàm, gọi nhiều lần như doạn code trên thì
+        //XANH: tất cả phải xanh
+        //ĐỎ: 1 TRONG ĐÁM ĐÓ BỊ ĐỎ, COI NHƯ CẢ ĐÁM ĐỎ
+        //màu xanh: khớp giữ expected và actual, do đó nếu có 1 cái k khớp
+        //màu đỏ -> chứng minh 1 điều làm lúc đúng lúc sai -> ko tốt
+        //để làm đc điều này ta cần 1 đồ chơi/thư việc phụ trợ
+        //để nó tự so sánh dùm expected và actual, in ra màu
+        //thư viện này là: JUnit, TestNG(java)
+        //                 xUnit, NUnit, MSUnit (C#)
+        //                 PHPUnit
+        //                 CPPUnit
+        //                  ...
+        //.jar, .dll, ...
     }
-    
+
 }
---
+++ test/tdd/MathUtilTest.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tdd;
+
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import util.MathUtil;
+
+/**
+ *
+ * @author SurfaceVN.com
+ */
+public class MathUtilTest {
+    //class này muốn chạy thì có hàm main() riêng, nhấn shift-F6 để chạy
+    //mỗi hàm trong class này sẽ biến thành main() // nhiều main() trong 1 class
+    //thì hàm cần phải có kí hiệu/chỉ đẫn/annotation @Test
+    @Test
+    public void testSccessfulCases(){
+        //test các tình huống tham số đưa cho hàm cần test 1 cách tử tế
+        //Assert.assertEquals(30, 40);
+        assertEquals(1, MathUtil.computeFactorial(0));
+        assertEquals(1, MathUtil.computeFactorial(1));
+        assertEquals(2, MathUtil.computeFactorial(2));
+        assertEquals(6, MathUtil.computeFactorial(3));
+        assertEquals(24, MathUtil.computeFactorial(4));
+        assertEquals(120, MathUtil.computeFactorial(5));
+        
+    }
+   
+    //main() khác
+    @Test(expected = IllegalArgumentException.class)
+    public void testFailfulCases(){
+        //test các tình huống đầu vào cà chớn
+        //ddieefu gif xayr ra neeus ng dùng muốn tính cF(-5)
+        //Xin lỗi tính k đc, quăng ra 1 message chửi ng dùng, đưa số âm sao tính
+        //ném ra 1 ngoại lệ/Exception bảo rằng: tham số/đầu vào vớ vẩn, cà chớn
+        //và ngoại lệ là 1 tình hướng bất thường k như dự tính
+        //và nó k ngoại lệ là 1 value để có thể so sánh đc
+        //k thể dùng hàm asserEquals()
+        MathUtil.computeFactorial(5);
+    }
+    
+    //hàm trả về màu đỏ do k ném về ngoại lệ như kì vọng
+    //code mình còn sai trong tình huống số âm, số dương đã màu xanh rồi
+    //-> hàm còn sai về logic, k sai cú pháp  , hàm cF();
+    //nhưng cho dù viết kiểu cũ sout() bên main hay asserE() bên JUnit
+    //thì code sai logic/màu đỏ vẫn k ngăn đc việc ta nhấn nút clean&build
+    //tức là code k sai cú pháp, vẫn thoải mái ra đc .jar, .war
+    //điều này k hay, k tốt
+    //JUnit có thể kết hợp vs ng kiến để cấm ra file .jar, .war NẾU CODE CÒN MÀU ĐỎ
+    //DISABLE NÚT BẤM CLEAN & BUILD NẾU MÀU ĐỎ VẪN CÒN TRNOG LÚC CHẠY
+    //@TEST
+    //HÃY NHỚ 2 CON SỐ: 1005 NETBEANS 8, 1204 NETBEAN 10 TRỬ LÊN
+    
+}
