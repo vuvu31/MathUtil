be93043586ff0c788ad153ce9968b25035b871b6
vuvu31 <vulambaovan31@gmail.com>
2020-07-23 14:07:09 +0700

    add exception handler

--- src/util/MathUtil.java
+++ src/util/MathUtil.java
@@ -14,10 +14,10 @@ public class MathUtil {
     //nên tràn int (2 tỉ 1) sớm, ta trả về long cỡ 15! hoy
     //mình chỉ nên tính n <= 15, vì 20! to lắm
     public static long computeFactorial(int n) {
-        /*//chặn cà chớn đưa vào, ném về ngoại lệ
+        //chặn cà chớn đưa vào, ném về ngoại lệ
         if(n < 0 || n >15){
-            throw new IllegalArgumentException("n must be >= 0 & n <= 15");
-        }*/
+            throw new IllegalArgumentException("Invalid argument. n must be >= 0 & n <= 15");
+        }
         //cứ viết, sai đúng từ từ tính
         long result = 1;
         for (int i = 1; i <= n; i++) {
